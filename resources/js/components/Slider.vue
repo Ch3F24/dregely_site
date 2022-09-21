0c755dcf0e6b199f750110450cbc353f93b98419
Chef <zsolt.horvath@wst.hu>
2022-09-21 22:07:14 +0200

    loading icon

--- resources/js/components/Slider.vue
+++ resources/js/components/Slider.vue
@@ -69,6 +69,12 @@
 
             </swiper-slide>
         </swiper>
+        <div class="loader absolute top-0 left-0 w-full h-full z-10 bg-white flex justify-center items-center" v-if="loading">
+            <svg class="animate-spin -ml-1 mr-3 h-10 w-10 text-dgrey" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </div>
     </div>
     <div class="relative md:flex items-center md:justify-center min-h-[10%] md:px-12">
         <div class="md:w-1/3 flex space-x-4 h-full items-center" v-if="withThumb && activeSlide && activeSlide.photos && activeSlide.photos.length > 1">
@@ -168,7 +174,8 @@ export default {
             thumbnailIndex: 0,
             activeSlide: [],
             open: false,
-            icon: '/assets/images/download.svg'
+            icon: '/assets/images/download.svg',
+            loading: true
         }
     },
     components: {Swiper, SwiperSlide, Modal},
@@ -180,6 +187,7 @@ export default {
                 class_loaded: 'lazy-loaded',
             });
             myLazyLoad.update();
+            this.loading = false
         })
     },
     methods: {
@@ -212,6 +220,7 @@ export default {
             return src + '?' + queryString;
         },
         onHorizontalSlideChange(swiper) {
+            this.loading = true
             this.thumbnailIndex = 0;
 
             let _this = this
@@ -221,6 +230,7 @@ export default {
                     _this.activeSlide = _this.slides[swiper.activeIndex]
                 }
             },300)
+            this.loading = false
         },
         selectThumbnail(key) {
             this.thumbnailIndex = key;
